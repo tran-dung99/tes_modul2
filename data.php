c5bf7dbeab7aee38a3cbf2d6c741c6f2305fec95
Dung <trandungxx2712@gmail.com>
2021-11-11 14:52:20 +0700

    commit file

---
+++ data.php
@@ -0,0 +1,19 @@
+<?php
+$array = [];
+function saveData($data)
+{
+    $dataJson = json_encode($data);
+    file_put_contents("used.json",$dataJson);
+}
+function loadData() {
+    $dataJson = file_get_contents("used.json");
+    $data = json_decode($dataJson,true);
+    return $data;
+}
+function addData($request) {
+    $array = loadData();
+    $number = $request["number"];
+    array_push($array,$number);
+    saveData($array);
+}
+
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include_once "data.php";
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <input type="number" name="number" placeholder="Nhập số bất kỳ">
+    <button type="submit">ADD</button>
+</form>
+</body>
+</html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $array = loadData();
+    addData($_POST);
+    sort($array);
+    print_r($array);
+}
+?>
+
